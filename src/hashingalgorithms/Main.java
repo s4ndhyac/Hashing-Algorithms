d5c2672cc42ea2da27541ece573eb7385bbe3dcc
s4ndhyac <s4ndhyac@gmail.com>
2019-02-17 16:31:54 -0800

    averaging values over no. of trials

--- src/hashingalgorithms/Main.java
+++ src/hashingalgorithms/Main.java
@@ -12,6 +12,7 @@ public class Main {
 
         int numSteps = Integer.parseInt("100");
         int stepSize = Integer.parseInt("50");
+        int avgTime = 100;
 
         double start, end, avgSet = 0, avgDelete = 0, avgSearch = 0;
         double loadFactor = 0;
@@ -22,107 +23,75 @@ public class Main {
 
         Random random = new Random();
         IHashingAlgorithm h;
-        switch ("1")
-        {
-            case "1":
-                h = new ChainedHashing(1000);
-                break;
-            case "2":
-                h = new LinearProbing(1000);
-                break;
-            case "3":
-                h = new Quadratic(1000);
-                break;
-            case "4":
-                h = new CuckooHashing(1000);
-                break;
-            default:
-                throw new IllegalArgumentException("Illegal function");
-        }
-
 
         for (int i = 1; i < numSteps; i++) {
             avgSet = 0;
             avgDelete = 0;
             avgSearch = 0;
             loadFactor = 0;
-            int range = i * stepSize;
+            for (int j = 0; j < avgTime; j++) {
+                switch ("4")
+                {
+                    case "1":
+                        h = new ChainedHashing(100000);
+                        break;
+                    case "2":
+                        h = new LinearProbing(100000);
+                        break;
+                    case "3":
+                        h = new QuadraticProbing(100000);
+                        break;
+                    case "4":
+                        h = new CuckooHashing(5000);
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Illegal function");
+                }
 
-            // generate random keys
-            List<Integer> list = new ArrayList<>(range);
-            for (int k = 0; k < range; k++) {
-                list.add(random.nextInt(1000000));
+                // generate random keys
+                List<Integer> list = new ArrayList<>(i*stepSize);
+                for (int k = 0; k < i*stepSize; k++) {
+                    list.add(random.nextInt(1000000));
+                }
+
+                start = System.currentTimeMillis();
+                for (int k = 0; k < i*stepSize; k++) {
+                    h.set(list.get(k), list.get(k));
+                }
+                end = System.currentTimeMillis();
+                avgSet += (end - start);
+                loadFactor += h.getLoadFactor();
+
+                Collections.shuffle(list);
+                start = System.currentTimeMillis();
+                for (int k = 0; k < stepSize && k < i*stepSize; k++) {
+                    h.search(list.get(k));
+                }
+                end = System.currentTimeMillis();
+                avgSearch += (end - start);
+
+                Collections.shuffle(list);
+                start = System.currentTimeMillis();
+                for (int k = 0; k < stepSize && k < i*stepSize; k++) {
+                    h.delete(list.get(k));
+                }
+                end = System.currentTimeMillis();
+                avgDelete += (end - start);
             }
 
-            start = System.currentTimeMillis();
-            for (int k = 0; k < range; k++) {
-                h.set(list.get(k), list.get(k));
-            }
-            end = System.currentTimeMillis();
-            avgSet += (end - start);
-            loadFactor += h.getLoadFactor();
-
-            Collections.shuffle(list);
-            start = System.currentTimeMillis();
-            for (int k = 0; k < range; k++) {
-                h.search(list.get(k));
-            }
-            end = System.currentTimeMillis();
-            avgSearch += (end - start);
-
-            Collections.shuffle(list);
-            start = System.currentTimeMillis();
-            for (int k = 0;  k < range; k++) {
-                h.delete(list.get(k));
-            }
-            end = System.currentTimeMillis();
-            avgDelete += (end - start);
-
-            loadFactors[i] = loadFactor;
+            loadFactors[i] = loadFactor/avgTime;
             System.out.println("i = " + i + ", loadFactor: " + loadFactors[i]);
 
-            setCosts[i] = avgSet;
+            setCosts[i] = avgSet / avgTime;
             System.out.println("i = " + i + ", set: " + setCosts[i]);
 
-            deleteCosts[i] = avgDelete;
+            deleteCosts[i] = avgDelete / avgTime;
             System.out.println("i = " + i + ", delete: " + deleteCosts[i]);
 
-            searchCosts[i] = avgSearch;
+            searchCosts[i] = avgSearch / avgTime;
             System.out.println("i = " + i + ", search: " + searchCosts[i]);
 
         }
-
-        System.out.println("");
-
-        System.out.println("Load Factors: [");
-        for(double i : loadFactors) {
-            System.out.print(i + ", ");
-        }
-        System.out.println("]");
-
-        System.out.println("");
-
-        System.out.println("Sets Sequence: [");
-        for(double i : setCosts) {
-            System.out.print(i + ", ");
-        }
-        System.out.println("]");
-
-        System.out.println("");
-
-        System.out.println("Delete: [");
-        for(double i : deleteCosts) {
-            System.out.print(i + ", ");
-        }
-        System.out.println("]");
-
-        System.out.println("");
-
-        System.out.println("Search: [");
-        for(double i : searchCosts) {
-            System.out.print(i + ", ");
-        }
-        System.out.println("]");
 
     }
 }
